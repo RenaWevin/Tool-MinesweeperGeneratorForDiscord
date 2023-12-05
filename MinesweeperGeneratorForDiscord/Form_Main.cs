024be8f52c43fe65f8655596ff4fb9b5e44d72a7
RenaWevin <renawevin@gmail.com>
2023-12-06 02:08:16 +0800

    [+] 實裝地雷生成過程(不含數字)

--- MinesweeperGeneratorForDiscord/Form_Main.cs
+++ MinesweeperGeneratorForDiscord/Form_Main.cs
@@ -12,7 +12,36 @@ namespace MinesweeperGeneratorForDiscord {
         public Form_Main() {
             InitializeComponent();
             UpdateDisplay_FloorCount();
+            MapCellTypeStrings.Add(MapCellType.Mine, "||:boom:||");
+            MapCellTypeStrings.Add(MapCellType.Empty, "||:zero:||");
+            MapCellTypeStrings.Add(MapCellType.Cell1, "||:one:||");
+            MapCellTypeStrings.Add(MapCellType.Cell2, "||:two:||");
+            MapCellTypeStrings.Add(MapCellType.Cell3, "||:three:||");
+            MapCellTypeStrings.Add(MapCellType.Cell4, "||:four:||");
+            MapCellTypeStrings.Add(MapCellType.Cell5, "||:five:||");
+            MapCellTypeStrings.Add(MapCellType.Cell6, "||:six:||");
+            MapCellTypeStrings.Add(MapCellType.Cell7, "||:seven:||");
+            MapCellTypeStrings.Add(MapCellType.Cell8, "||:eight:||");
         }
+
+        private readonly Dictionary<MapCellType, string> MapCellTypeStrings = new Dictionary<MapCellType, string>();
+
+        #region enum
+
+        private enum MapCellType {
+            Mine = -1,
+            Empty = 0,
+            Cell1 = 1,
+            Cell2 = 2,
+            Cell3 = 3,
+            Cell4 = 4,
+            Cell5 = 5,
+            Cell6 = 6,
+            Cell7 = 7,
+            Cell8 = 8,
+        }
+
+        #endregion
 
         #region 左上角目錄選單按鈕事件
 
@@ -82,7 +111,41 @@ namespace MinesweeperGeneratorForDiscord {
                 return;
             }
             StringBuilder sb = new StringBuilder();
-            //生成過程★
+            #region  -> 生成過程
+            //生成過程
+            int[] map = new int[floorCount];
+            int[] mapRandomIds = new int[floorCount];
+            for (int i = 0; i < map.Length; i++) {
+                mapRandomIds[i] = i;
+            }
+            //隨機取出要生成地雷的Index
+            Random random = new Random();
+            for (int i = 0; i < mineCount; i++) {
+                int randomMineMax = floorCount - i;
+                int randomIndex = random.Next(0, randomMineMax);
+                int randomId = mapRandomIds[randomIndex];
+                //設定該格為地雷
+                map[randomId] = (int)MapCellType.Mine;
+                //將最後一項的數值放進這項，直接取代掉就好 
+                mapRandomIds[randomIndex] = mapRandomIds[randomMineMax - 1];
+            }
+            //為所有有地雷的周邊加上數字
+            for (int i = 0; i < map.Length; i++) {
+                //★
+            }
+            #endregion
+            #region  -> 將整張地圖轉成文字
+            sb.AppendLine($"地圖大小 {floorX}x{floorY}");
+            sb.AppendLine($"地雷數量 {mineCount}");
+            for (int i = 0; i < map.Length; i++) {
+                if (i > 0 && i % floorX == 0) {
+                    sb.AppendLine();
+                }
+                MapCellType type = (MapCellType)map[i];
+                string cell = MapCellTypeStrings[type];
+                sb.Append(cell);
+            }
+            #endregion
             textBox_Output.Text = sb.ToString();
         }
 
